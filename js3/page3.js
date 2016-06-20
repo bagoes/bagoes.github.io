9aac3843ca2107c29037f6c4f501ccaf15504740
bagoes <bagoessoemargo@gmail.com>
2016-06-20 13:54:13 +0700

    Upload 3

---
+++ js3/page3.js
@@ -0,0 +1,4 @@
+/* When your mouse cursor enter the background, the fading won't pause and keep playing */ 
+$('.carousel').carousel({
+    pause: "false" /* Change to true to make it paused when your mouse cursor enter the background */
+});
