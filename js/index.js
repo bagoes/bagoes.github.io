2e024b2378f640dc4fe878ad3b334ed6a5ee4f35
bagoes <bagoessoemargo@gmail.com>
2019-09-03 15:09:37 +0700

    Delete index.js

--- js/index.js
+++
@@ -1,4 +0,0 @@
-/* When your mouse cursor enter the background, the fading won't pause and keep playing */ 
-$('.carousel').carousel({
-    pause: "false" /* Change to true to make it paused when your mouse cursor enter the background */
-});
